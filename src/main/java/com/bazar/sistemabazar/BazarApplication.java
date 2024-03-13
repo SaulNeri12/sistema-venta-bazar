bb039c07092098dec2c522337ff78bb5db7a1d2c
Saul Neri <saul.neri247498@potros.itson.edu.mx>
2024-03-13 01:02:31 -0700

    casi termino el diseno del login

--- src/main/java/com/bazar/sistemabazar/BazarApplication.java
+++ src/main/java/com/bazar/sistemabazar/BazarApplication.java
@@ -2,8 +2,12 @@ package com.bazar.sistemabazar;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.Screen;
 
 import java.io.IOException;
 
@@ -11,12 +15,50 @@ public class BazarApplication extends Application {
     @Override
 		// cambio
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(BazarApplication.class.getResource("app.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Sistema Bazar");
-        stage.setMaximized(true);
+
+        // se mostrara el login
+        FXMLLoader loginFxmlLoader = new FXMLLoader(BazarApplication.class.getResource("Login.fxml"));
+        Parent root = loginFxmlLoader.load();
+        Stage loginStage = new Stage();
+
+        LoginController loginController = loginFxmlLoader.getController();
+        loginController.setStage(loginStage);
+
+        // si se cierra la ventana del login, no se abre la ventana de la aplicacion
+        // y se termina el programa
+        loginStage.setOnCloseRequest(event -> {
+            loginStage.close();
+            System.exit(0);
+        });
+
+        loginStage.setTitle("Login");
+        loginStage.setScene(new Scene(root));
+
+        loginStage.initModality(Modality.APPLICATION_MODAL);
+        loginStage.showAndWait();
+        centerStage(loginStage);
+
+        // se mostrara el login
+        FXMLLoader bazarFxmlLoader = new FXMLLoader(BazarApplication.class.getResource("Login.fxml"));
+        Parent bazarRoot = bazarFxmlLoader.load();
+        Scene scene = new Scene(bazarRoot);
         stage.setScene(scene);
+        stage.setTitle("Sistema Bazar");
+
         stage.show();
+    }
+
+    private void centerStage(Stage stage) {
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+
+        double stageWidth = stage.getWidth();
+        double stageHeight = stage.getHeight();
+
+        double centerX = (primaryScreenBounds.getWidth() - stageWidth) / 2;
+        double centerY = (primaryScreenBounds.getHeight() - stageHeight) / 2;
+
+        stage.setX(centerX);
+        stage.setY(centerY);
     }
 
     public static void main(String[] args) {
--- src/main/java/com/bazar/sistemabazar/LoginController.java
+++ src/main/java/com/bazar/sistemabazar/LoginController.java
@@ -1,10 +1,62 @@
 package com.bazar.sistemabazar;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class LoginController {
 
+    private Stage stage;
+
+    @FXML
     public TextField loginPhoneField;
+    @FXML
     public PasswordField loginPasswordField;
+    @FXML
+    public Button loginButton;
+
+    @FXML
+    public void onLoginBtnClick() {
+        Pattern phoneNumberPattern = Pattern.compile("\\d{10,}-?\\d*$");
+        Pattern passwordPattern = Pattern.compile("admin");
+
+        boolean validPhoneNumber = phoneNumberPattern.matcher(loginPhoneField.getText()).matches();
+
+        if (!validPhoneNumber) {
+            showLoginErrorMessage("Telefono Invalido", "El numero de telefono dado no es valido.");
+            return;
+        }
+
+        boolean validPassword = passwordPattern.matcher(loginPasswordField.getText()).matches();
+
+        if (!validPassword) {
+            showLoginErrorMessage("Contrasena Invalida", "La contraseña no es valida, la contrasena debe ser de al menos 8 caracteres y contener al menos un numero.");
+        }
+
+        this.close();
+    }
+
+    public void setStage(Stage stage) {
+        if (stage != null) {
+            this.stage = stage;
+        }
+    }
+
+    public void close() {
+        this.stage.close();
+    }
+
+    public void showLoginErrorMessage(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        // Mostrar la alerta
+        alert.showAndWait();
+    }
 }
