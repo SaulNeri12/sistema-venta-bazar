5979921da0423b819b7ae9d070a14fa7b48b378f
Saul Neri <saul.neri247498@potros.itson.edu.mx>
2024-03-12 22:31:19 -0700

    se anadio el login para empezar a trabajar con el

---
+++ src/main/java/com/bazar/sistemabazar/LoginController.java
@@ -0,0 +1,10 @@
+package com.bazar.sistemabazar;
+
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    public TextField loginPhoneField;
+    public PasswordField loginPasswordField;
+}
