eb44efa8dc5d4a481e4097204db951097faa4a51
Saul Neri <saul.neri247498@potros.itson.edu.mx>
2024-03-18 16:46:05 -0700

    Se mejoro el PanelVenta

--- src/main/java/com/bazar/sistemabazar/components/tables/ProductoVentaTableView.java
+++ src/main/java/com/bazar/sistemabazar/components/tables/ProductoVentaTableView.java
@@ -1,14 +1,12 @@
 package com.bazar.sistemabazar.components.tables;
 
-import javafx.beans.property.SimpleFloatProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleLongProperty;
-import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableCell;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 
 public class ProductoVentaTableView extends TableView<ProductoVentaTableView.ProductoVentaTableModel> {
@@ -20,24 +18,29 @@ public class ProductoVentaTableView extends TableView<ProductoVentaTableView.Pro
         this.getStyleClass().add("tabla-venta");
 
         // se crean las columnas necesarias de la tabla
-        TableColumn<ProductoVentaTableModel, Long> columnaProductoId = new TableColumn<>("ID");
+        TableColumn<ProductoVentaTableModel, Integer> columnaProductoId = new TableColumn<>("ID");
         columnaProductoId.setPrefWidth(100);
+        columnaProductoId.setCellValueFactory(cellData -> cellData.getValue().getIdProperty().asObject());
 
         TableColumn<ProductoVentaTableModel, String> columnaNombreProducto = new TableColumn<>("Nombre del producto");
         columnaNombreProducto.setPrefWidth(250);
+        columnaNombreProducto.setCellValueFactory(cellData -> cellData.getValue().getNombreProperty());
 
         TableColumn<ProductoVentaTableModel, Float> columnaPrecioProducto = new TableColumn<>("Precio");
         columnaPrecioProducto.setPrefWidth(100);
+        columnaPrecioProducto.setCellValueFactory(cellData -> cellData.getValue().getPrecioProperty().asObject());
 
         TableColumn<ProductoVentaTableModel, Integer> columnaCantidadProducto = new TableColumn<>("Cantidad");
         columnaCantidadProducto.setPrefWidth(100);
+        columnaCantidadProducto.setCellValueFactory(cellData -> cellData.getValue().getCantidadProperty().asObject());
 
         TableColumn<ProductoVentaTableModel, Float> columnaTotalProducto = new TableColumn<>("Total");
         columnaTotalProducto.setPrefWidth(100);
+        columnaTotalProducto.setCellValueFactory(cellData -> cellData.getValue().getTotalProperty().asObject());
 
         ObservableList<ProductoVentaTableModel> listaProductos = FXCollections.observableArrayList(
-                new ProductoVentaTableModel((long) 100000, "Alice Akerman", 40.21f, 1, 40.21f),
-                new ProductoVentaTableModel((long) 1000001, "Saul Armando Neri Escarcega", 20.21f, 5, 101.05f)
+                new ProductoVentaTableModel(12, "Ruffles Queso (100g)", 40.21f, 1, 40.21f),
+                new ProductoVentaTableModel(13, "Galletas Emperador", 20.21f, 5, 101.05f)
         );
 
         this.getItems().addAll(listaProductos);
@@ -57,21 +60,21 @@ public class ProductoVentaTableView extends TableView<ProductoVentaTableView.Pro
     }
 
     public static class ProductoVentaTableModel {
-        private SimpleLongProperty productoId;
+        private SimpleIntegerProperty productoId;
         private SimpleStringProperty nombre;
         private SimpleFloatProperty precio;
         private SimpleIntegerProperty cantidad;
         private SimpleFloatProperty total;
 
-        public ProductoVentaTableModel(long productoId, String nombre, float precio, int cantidad, float total) {
-            this.productoId = new SimpleLongProperty(productoId);
+        public ProductoVentaTableModel(int productoId, String nombre, float precio, int cantidad, float total) {
+            this.productoId = new SimpleIntegerProperty(productoId);
             this.nombre = new SimpleStringProperty(nombre);
             this.precio = new SimpleFloatProperty(precio);
             this.cantidad = new SimpleIntegerProperty(cantidad);
             this.total = new SimpleFloatProperty(total);
         }
 
-        public void setId(long productoId) {
+        public void setId(int productoId) {
             this.productoId.set(productoId);
         }
 
@@ -81,15 +84,40 @@ public class ProductoVentaTableView extends TableView<ProductoVentaTableView.Pro
 
         public void setPrecio(float precio) {
             this.precio.set(precio);
+            this.calcularTotal();
         }
 
-        public void setCantidad(int cantidad) {
-            this.cantidad.set(cantidad);
+        private void calcularTotal() {
+            float total = this.precio.get() * this.cantidad.get();
+            this.total.set(total);
         }
 
         public void setTotal(float total) {
+            calcularTotal();
             this.total.set(total);
         }
+
+        public IntegerProperty getIdProperty() {
+            return this.productoId;
+        }
+
+        public StringProperty getNombreProperty() {
+            return this.nombre;
+        }
+
+        public FloatProperty getPrecioProperty() {
+            return this.precio;
+        }
+
+        public IntegerProperty getCantidadProperty() {
+            return this.cantidad;
+        }
+
+        public FloatProperty getTotalProperty() {
+            return this.total;
+        }
+
+
 
         public long getId() {
             return this.productoId.get();
