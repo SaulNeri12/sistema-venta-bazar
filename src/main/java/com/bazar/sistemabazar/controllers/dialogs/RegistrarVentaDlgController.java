4da1c4a2c06d6a74c1e57b597e4a86e4dccb87a9
Saul Neri <saul.neri247498@potros.itson.edu.mx>
2024-04-08 22:32:17 -0700

    Cree la vista RegistrarVentaDlg

---
+++ src/main/java/com/bazar/sistemabazar/controllers/dialogs/RegistrarVentaDlgController.java
@@ -0,0 +1,15 @@
+package com.bazar.sistemabazar.controllers.dialogs;
+
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
+public class RegistrarVentaDlgController {
+    public TextField nombreClienteTextField;
+    public Label apellidoClienteTextField;
+    public ChoiceBox metodoPagoChoiceBox;
+    public TextField montoTotalTextField;
+    public Spinner campoMontoPagoSpinner;
+    public TextField campoCambioTextField;
+}
